197111c5c5e46ef323c9f1930c2ad7bfc1476b05
uniqueneu3900 <62101934+unique3900@users.noreply.github.com>
2023-07-23 21:20:01 +0545

    Code Refactor on order display (Multiple Item on same order was not being diaplayed)

--- Client/src/features/user/Components/UserOrders.jsx
+++ Client/src/features/user/Components/UserOrders.jsx
@@ -33,6 +33,10 @@ const UserOrders = () => {
             ! user && <Navigate to={'/login'}
                 replace={true}></Navigate>
         }
+            {
+            ! orders && <Navigate to={'/'}
+                replace={true}></Navigate>
+        }
 
 
             <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-5'>
@@ -43,8 +47,8 @@ const UserOrders = () => {
                         <ul role="list" className="-my-6 divide-y divide-gray-200">
                             {
                             orders.map((orders) => (
-                                <>
-                                    <div className="flex flex-row justify-between">
+                                <div key={orders.id}>
+                                    <div  className="flex flex-row justify-between">
 
                                         <p className="p-5 text-3xl font-bold text-purple-700 ">Order id: #{
                                             orders.id
@@ -54,67 +58,74 @@ const UserOrders = () => {
                                         }</p>
                                     </div>
 
-                                    <li key={
-                                            orders.id
-                                        }
-                                        className="flex py-6">
-
-                                        <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-
-                                            <img src={
-                                                    orders.items[0].thumbnail
-                                                }
-                                                alt={
-                                                    orders.items[0].title
-                                                }
-                                                className="h-full w-full object-cover object-center"/>
-                                        </div>
-
-                                        <div className="ml-4 flex flex-1 flex-col">
-                                            <div>
-                                                <div className="flex justify-between text-base font-medium text-gray-900">
-                                                    <h3>
-                                                        <p> {
-                                                            orders.items[0].title
-                                                        }</p>
-                                                    </h3>
-                                                    <div className="flex flex-col justify-center gap-2">
-                                                        <p className="ml-4">
-                                                            NPR &nbsp; {
-                                                            orders.totalAmount
-                                                        }</p>
-                                                        <p className="ml-4">Payment Method : {orders.selectedPaymentMethod
-}</p>
-                                                    </div>
-
-
-                                                </div>
-                                                <p className="mt-1 text-sm text-gray-500">
-                                                    {
-                                                    orders.color
-                                                }</p>
-                                            </div>
-                                            <div className="flex flex-1 items-end justify-between text-sm">
-                                                <div className="flex flex-col jc items-start gap-2">
-                                                    <p className="text-gray-500">Qty : {
-                                                        orders.totalItems
-                                                    }</p>
-                                                    <p className="text-gray-500">Address:{
-                                                        orders.selectedDeliveryAddress.selectedState + " " + orders.selectedDeliveryAddress.selectedCity + " " + orders.selectedDeliveryAddress.selectedLocation + " " + orders.selectedDeliveryAddress.street
-                                                    } </p>
-                                                    <p className="text-gray-500">Receiver: {
-                                                        orders.selectedDeliveryAddress.fullName
+                                    {
+                                        orders.items.map((orderItems) => (
+                                            <li key={
+                                                orderItems.id
+                                            }
+                                            className="flex py-6">
+                                         
+                                            <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+    
+                                                <img src={
+                                                        orderItems.thumbnail
                                                     }
-                                                        ({
-                                                        orders.selectedDeliveryAddress.phone
-                                                    })</p>
-                                                </div>
-
-
+                                                    alt={
+                                                        orderItems.title
+                                                    }
+                                                    className="h-full w-full object-cover object-center"/>
                                             </div>
-                                        </div>
-                                    </li>
-                                </>
+    
+                                            <div className="ml-4 flex flex-1 flex-col">
+                                                <div>
+                                                    <div className="flex justify-between text-base font-medium text-gray-900">
+                                                        <h3>
+                                                            <p> {
+                                                                orderItems.title
+                                                            }</p>
+                                                        </h3>
+                                                        <div className="flex flex-col justify-center gap-2">
+                                                            <p className="ml-4">
+                                                                NPR &nbsp; {
+                                                                orderItems.price
+                                                            }</p>
+                                                            <p className="ml-4">Payment Method : {orders.selectedPaymentMethod
+    }</p>
+                                                        </div>
+    
+    
+                                                    </div>
+                                                    <p className="mt-1 text-sm text-gray-500">
+                                                        {
+                                                        orders.color
+                                                    }</p>
+                                                </div>
+                                                <div className="flex flex-1 items-end justify-between text-sm">
+                                                    <div className="flex flex-col jc items-start gap-2">
+                                                        <p className="text-gray-500">Qty : {
+                                                            orderItems.quantity
+                                                        }</p>
+                                                        <p className="text-gray-500">Address:{
+                                                            orders.selectedDeliveryAddress.selectedState + " " + orders.selectedDeliveryAddress.selectedCity + " " + orders.selectedDeliveryAddress.selectedLocation + " " + orders.selectedDeliveryAddress.street
+                                                        } </p>
+                                                        <p className="text-gray-500">Receiver: {
+                                                            orders.selectedDeliveryAddress.fullName
+                                                        }
+                                                            ({
+                                                            orders.selectedDeliveryAddress.phone
+                                                        })</p>
+                                                    </div>
+    
+    
+                                                </div>
+                                            </div>
+                                        </li>
+                                        ))
+                                    }
+
+
+                                    
+                                </div>
 
                             ))
                         } </ul>
