23b61d0b7bd855ee732076668ca027387146fdca
parashar <parasharneupane3900@gmail.com>
2023-10-09 18:49:45 +0545

    Backend Restarted

--- Server/index.js
+++ Server/index.js
@@ -1,26 +1,28 @@
 const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const bcrypt = require('bcrypt');
-const { LoginController } = require('./Controller/AuthController');
-const { default: mongoose } = require('mongoose');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
-const app = express();
-
-app.use(express.json({ limit: 52428800 }));
-app.use(morgan('dev'));
-app.use(cors({
-    origin: 'http://127.0.0.1:5173',
-    credentials: true,
-}));
-mongoose.connect(process.env.MONGODB_URL);
-
-const authRoutes = require('./Routes/authRoutes');
-app.use('/api/v1/auth',authRoutes)
+const cors = require('cors');
 
 
+async function main() {
+    await mongoose.connect("mongodb://localhost:27017/ParasharShop").catch((err) => {
+        console.log("Error Occured On Database Connection", err);
+    })
+    
+    // console.log("Database Connection Successful");
+}
+main();
 
-const port=process.env.PORT
-app.listen(port, () => {
-    console.log("Server started at port" + port);
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+
+const port = process.env.PORT;
+console.log(port)
+server.listen(port, () => {
+    console.log("Server Started at port", port);
 })
+
+
+
