dbc3e8685f404e050960891dc1a5ca8f644411a9
parashar <parasharneupane3900@gmail.com>
2023-10-17 14:29:17 +0545

    Backend for updating user and password encrypted

--- Server/controllers/AuthController.js
+++ Server/controllers/AuthController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { User } = require('../models/User');
+const bcrypt = require('bcrypt');
 const app = express();
 
 exports.LoginController = async (req, res) => {
@@ -8,13 +9,13 @@ exports.LoginController = async (req, res) => {
         res.status(401).json({success:false,message:"Email and Password is Required"})
     }
     else {
-        const user = await User.findOne( {email} );
+        const user = await User.findOne({ email });
         if (!user) {
             res.status(401).json({success:false,message:"User Doesnot Exist"})
         }
         else {
-            console.log(user.email)
-            if (user.password === password) {
+            
+            if ( bcrypt.compareSync(password,user.password)) {
                 res.status(200).json({success:true,message:"User Authenticated",user})
             }
             else {
@@ -26,11 +27,24 @@ exports.LoginController = async (req, res) => {
 }
 exports.RegisterController = async (req, res) => {
     try {
-        console.log(req.body)
-        const users = await User.create(req.body);
+        const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+        const users = await User.create({...req.body,password:hashedPassword});
         res.json({success:true,message:"User Registration Successful",users})
     } catch (error) {
+        console.log(error)
         res.status(400).json({ success: false, message: "Error in Registering User", error })
     }
 
+}
+
+exports.updateUserController = async (req, res) => {
+    const { id } = req.body;
+    try {
+        const user = await User.findByIdAndUpdate(id,{...req.body},{new:true})
+        res.status(200).json({success:true,message:"User Updated Successfully",user})
+        
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({success:false,message:"Unexpected Error Occured While Updating User"})
+    }
 }
--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const { LoginController, RegisterController } = require('../controllers/AuthController');
+const { LoginController, RegisterController, updateUserController } = require('../controllers/AuthController');
 
 const router = express.Router();
 
 router.post('/register', RegisterController)
-    .post('/login', LoginController);
+    .post('/login', LoginController).post('/update',updateUserController)
 
 
 exports.router = router;
