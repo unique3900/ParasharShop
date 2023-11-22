6d5abd97c15360d0f463f4be76f541cbd76c2d3e
parashar <parasharneupane3900@gmail.com>
2023-11-22 08:24:23 +0545

    Upload Folder Refactoration

---
+++ Server/Image/uploadRoute.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const imageDownloader = require('image-downloader');
+const multer = require('multer');
+const fs=require('fs')
+const router = express.Router();
+
+// If Post -> /upload then upload by url and if / then upload local
+const photoMiddleware = multer({dest:'uploads/'});
+
+router.post('/', photoMiddleware.array('photos', 4), async (req, res) => {
+    try {
+        const uploadedFiles = [];
+        for (var i = 0; i <= req.files.length; i++){
+            const {path,originalname} = req.files[i];
+            const parts = originalname.split('.');
+            const extension = parts[parts.length - 1];
+            const newPath = path + '.' + extension;
+            fs.renameSync(path, newPath);
+            uploadedFiles.push(newPath.replace('uploads\\',''));
+        }
+        res.status(200).json({success:true,message:"Local Image Uploaded Successfully",uploadedFiles})
+    } catch (error) {
+        res.status(401).json({success:false,message:'Uploading Local Image Failed',error})
+        console.log(error)
+    }
+}).post('/upload-link', async (req, res) => {
+    try {
+        const { imageURL } = req.body;
+        const newName = 'photo' + Date.now() + '.jpg';
+        await imageDownloader.image({
+            url: imageURL,
+            dest: __dirname + '/uploads/' + newName
+        })
+        res.status(200).json({success:true,message:"Image Uploaded Successfully",newName})
+    } catch (error) {
+        res.status(401).json({success:false,message:"Image Upload Failed",error})
+        console.log(error)
+    }
+})
+
+
+exports.router = router;
--- Server/index.js
+++ Server/index.js
@@ -12,10 +12,11 @@ const userRouter = require('./routes/userRoutes');
 const cartRouter = require('./routes/cartRoute');
 const sellerRouter = require('./routes/sellerRoute');
 const orderRouter = require('./routes/orderRoutes');
-const uploadRouter = require('./routes/uploadRoute');
+const uploadRouter = require('./Image/uploadRoute');
 const morgan = require('morgan');
 const multer = require('multer');
 const fs = require('fs');
+const imageDownloader = require('image-downloader');
 
 async function main() {
     await mongoose.connect("mongodb://localhost:27017/ParasharShop")
@@ -30,6 +31,7 @@ main().catch((err) => {
 // ================= Initialization =====================
 
 const server = express();
+
 server.use(morgan('dev'))
 server.use(express.json());
 server.use(cors({
@@ -47,35 +49,12 @@ server.use('/brands', brandRouter.router);
 server.use('/cart', cartRouter.router);
 server.use('/seller',sellerRouter.router );
 server.use('/orders', orderRouter.router);
+server.use('/upload', uploadRouter.router);
 
 
 // Handling Image Upload Operation Here
 // Seperate router has been created aswell
-const photoMiddleware = multer({dest:'uploads/'});
 
-server.post('/uploads/', photoMiddleware.array('photos', 4), async (req, res) => {
-    try {
-        const uploadedFiles = [];
-        for (var i = 0; i <= req.files.length; i++){
-            const {path,originalname} = req.files[i];
-            const parts = originalname.split('.');
-            const extension = parts[parts.length - 1];
-            const newPath = path + '.' + extension;
-            fs.renameSync(path, newPath);
-            uploadedFiles.push(newPath.replace('uploads\\',''));
-        }
-        res.status(200).json({success:true,message:"Local Image Uploaded Successfully",uploadedFiles})
-    } catch (error) {
-        res.status(401).json({success:false,message:'Uploading Local Image Failed',error})
-        console.log(error)
-    }
-}).post('/upload-link', async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-})
 
 
 
