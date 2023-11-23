adbdafaae41186df5ac6e41dbca11d0ca4c6fb11
parashar <parasharneupane3900@gmail.com>
2023-11-23 17:52:12 +0545

    Mini Pagination Implemented For Seller Order Listing

--- Client/src/Components/Layout/Pagination.jsx
+++ Client/src/Components/Layout/Pagination.jsx
@@ -17,4 +17,21 @@ const Pagination = ({page,setPage,totalPage}) => {
   )
 }
 
+export const MiniPagination = ({page,setPage,totalPage}) => {
+  return (
+    <div className='w-full mt-10 flex flex-row justify-between px-10 '  >
+          <button className={`font-bold text-4xl text-purple-700`} disabled={page==1?true:false} onClick={() => {
+                  page==1?setPage(1):setPage(page-1)
+          }}>⬅️</button>
+          <div className="flex items-center">
+              <p className="italic">{page} out of {Math.ceil(totalPage/4) } pages</p>
+              <button className={`font-bold text-4xl`} onClick={() => {
+                  page==totalPage/4?setPage(Math.ceil(totalPage/4) ):setPage(page+1)
+          }} disabled={page== Math.ceil(totalPage/4)?true:false}>➡️</button>
+          </div>
+          
+    </div>
+  )
+}
+
 export default Pagination
--- Client/src/Components/Pages/Seller/OrderManage.jsx
+++ Client/src/Components/Pages/Seller/OrderManage.jsx
@@ -1,174 +1,158 @@
-import React, {
-    useEffect, useState
-} from 'react'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectLoggedInSeller } from "../../../features/Auth/authSlice";
 import {
-    useDispatch,
-    useSelector
-} from 'react-redux'
-import {
-    selectLoggedInSeller
-} from '../../../features/Auth/authSlice'
-import {
-    fetchOrderForSellerAsync,
-    selectSellerOrder,
-    updateOrderAsync
-} from '../../../features/order/orderSlice';
-import { discountedPrice } from '../../../app/constants';
-import { AiOutlineEdit,AiOutlineEye } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
-import Pagination from '../../Layout/Pagination';
+  fetchOrderForSellerAsync,
+  selectSellerOrder,
+  updateOrderAsync,
+} from "../../../features/order/orderSlice";
+import { discountedPrice } from "../../../app/constants";
+import { AiOutlineEdit, AiOutlineEye } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import Pagination, { MiniPagination } from "../../Layout/Pagination";
 const OrderManage = () => {
-    const seller = useSelector(selectLoggedInSeller);
-    const sellerOrders = useSelector(selectSellerOrder);
-    const dispatch = useDispatch();
-    const [editable, setEditable] = useState(-1);
-    const [page, setPage] = useState(1);
-    const handleShow = (order) => {
-        
-    }
-    const handleEdit = (e, order,index, id) => {
-        const data = { value: e.target.value,index:index, id: id, order }
-        dispatch(updateOrderAsync(data))
-        setEditable(-1);
-    }
-    useEffect(() => {
-        dispatch(fetchOrderForSellerAsync(seller.id))
-    }, [dispatch,editable])
+  const seller = useSelector(selectLoggedInSeller);
+  const sellerOrders = useSelector(selectSellerOrder);
+  const dispatch = useDispatch();
+  const [editable, setEditable] = useState(-1);
+  const [page, setPage] = useState(1);
+  const handleShow = (order) => {};
+  const handleEdit = (e, order, index, id) => {
+    const data = { value: e.target.value, index: index, id: id, order };
+    dispatch(updateOrderAsync(data));
+    setEditable(-1);
+  };
+  useEffect(() => {
+    dispatch(fetchOrderForSellerAsync(seller.id));
+  }, [dispatch, editable]);
 
-    return (
-        <> {/* component */}
-            <div className="overflow-scroll">
-                <h2 className="text-center font-bold text-4xl p-3">Manage Orders</h2>
-                <div className=" bg-gray-100 w-full flex  items-center justify-center font-sans overflow-hidden">
-                    <div className="overflow-auto no-scrollbar">
-                        <div className=" bg-white w-screen min-w-max lg:min-w-fit lg:w-fit shadow-md rounded my-6 p-5">
-                            <table className=" table-auto">
-                                <thead className=''>
-                                    <tr className="bg-gray-200  text-gray-600 uppercase text-sm leading-normal ">
-                                        <th className="py-3 px-6 text-left">Order Id</th>
-                                        <th className="py-3 px-6 text-left">Items</th>
-                                        <th className="py-3 px-6 text-center">Product Detail</th>
-                                        <th className="py-3 px-6 text-center">Shipping</th>
-                                        <th className="py-3 px-6 text-center">Total Amount</th>
-                                        <th className="py-3 px-6 text-center">Status</th>
-                                        <th className="py-3 px-6 text-center">Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody className="text-gray-600 text-sm font-light">
-                                    {
-                                    sellerOrders.slice(page*12-12,page*12).map((item, index) => (
-                                        <tr key={
-                                                index
-                                            }
-                                            className="border-b border-gray-200 hover:bg-gray-100">
-                                            <td className="py-3 px-6 text-center whitespace-nowrap">
-                                                <div className="flex flex-col items-center justify-center">
+  return (
+    <>
+      {" "}
+      {/* component */}
+      <div className="overflow-scroll">
+        <h2 className="text-center font-bold text-4xl p-3">Manage Orders</h2>
+        <div className=" bg-gray-100 w-full flex  items-center justify-center font-sans overflow-hidden">
+          <div className="overflow-auto no-scrollbar">
+            <div className=" bg-white w-screen min-w-max lg:min-w-fit lg:w-fit shadow-md rounded my-6 p-5">
+              <table className=" table-auto">
+                <thead className="">
+                  <tr className="bg-gray-200  text-gray-600 uppercase text-sm leading-normal ">
+                    <th className="py-3 px-6 text-left">Order Id</th>
+                    <th className="py-3 px-6 text-left">Items</th>
+                    <th className="py-3 px-6 text-center">Product Detail</th>
+                    <th className="py-3 px-6 text-center">Shipping</th>
+                    <th className="py-3 px-6 text-center">Total Amount</th>
+                    <th className="py-3 px-6 text-center">Status</th>
+                    <th className="py-3 px-6 text-center">Actions</th>
+                  </tr>
+                </thead>
 
-                                                    <span className="font-medium">
-                                                       # {
-                                                        item.id
-                                                    }</span>
-                                                </div>
-                                                </td>
-                                                <td className="py-3 px-6 text-center  whitespace-nowrap">
-                                                <div className="flex flex-col items-center justify-center">
-                                                    <span className="font-medium">
-                                                        {
-                                                      item.products.length
-                                                    }</span>
-                                                </div>
-                                            </td>
+                <tbody className="text-gray-600 text-sm font-light">
+                  {sellerOrders
+                    .slice(page * 4 - 4, page * 4)
+                    .map((item, index) => (
+                      <tr
+                        key={index}
+                        className="border-b border-gray-200 hover:bg-gray-100"
+                      >
+                        <td className="py-3 px-6 text-center whitespace-nowrap">
+                          <div className="flex flex-col items-center justify-center">
+                            <span className="font-medium"># {item.id}</span>
+                          </div>
+                        </td>
+                        <td className="py-3 px-6 text-center  whitespace-nowrap">
+                          <div className="flex flex-col items-center justify-center">
+                            <span className="font-medium">
+                              {item.products.length}
+                            </span>
+                          </div>
+                        </td>
 
-                                            {item.products.map((data, ind) => (
-                                                 <div className='flex flex-col items-center justify-center'>
-                                                     <td className="py-3 px-6 text-center whitespace-nowrap">
-                                                       <div className="flex flex-col items-center justify-center">
-                                                            <img className='w-10 h-10'src={data.product.thumbnail} alt=""/>
-                                                               <span className="font-medium">
-                                                                    {data.product.title}
-                                                                    (Quantity: {data.quantity})
-                                                                 </span>
-                                                              </div>
-                                                          </td>
-                                              </div>
-                                            ))}
+                        {item.products.map((data, ind) => (
+                          <div className="flex flex-col items-center justify-center">
+                            <td className="py-3 px-6 text-center whitespace-nowrap">
+                              <div className="flex flex-col items-center justify-center">
+                                <img
+                                  className="w-10 h-10"
+                                  src={data.product.thumbnail}
+                                  alt=""
+                                />
+                                <span className="font-medium">
+                                  {data.product.title}
+                                  (Quantity: {data.quantity})
+                                </span>
+                              </div>
+                            </td>
+                          </div>
+                        ))}
+                        <td className="py-3 px-6 text-center whitespace-nowrap">
+                          <div className="flex flex-col items-center ">
+                            <span className="font-medium text-xs">
+                              {item.selectedDeliveryAddress.fullName}
+                            </span>
+                            <span className="font-medium text-xs">
+                              {item.selectedDeliveryAddress.email}
+                            </span>
+                            <span className="font-medium text-xs">
+                              {item.selectedDeliveryAddress.phone}
+                            </span>
+                            <span className="font-medium text-xs">
+                              {item.selectedDeliveryAddress.selectedState}{" "}
+                              &nbsp;
+                              {item.selectedDeliveryAddress.selectedCity} &nbsp;
+                              {
+                                item.selectedDeliveryAddress.selectedLocation
+                              }{" "}
+                              &nbsp;
+                              {item.selectedDeliveryAddress.street}&nbsp;
+                              {item.selectedDeliveryAddress.houseNumber}
+                            </span>
+                            <span className="bg-teal-500 px-3 py-2 mt-2 font-bold text-white">
+                              Payment:{item.selectedPaymentMethod}
+                            </span>
+                          </div>
+                        </td>
 
-                                            {}
-                                            
-                                
-                                            <td className="py-3 px-6 text-center whitespace-nowrap">
-                                                <div className="flex flex-col items-center ">
+                        <td className="py-3 px-6 text-center  whitespace-nowrap">
+                          <div className="flex flex-col gap-5 items-center justify-center">
+                            <span className="font-medium">
+                              {item.totalAmount}
+                            </span>
+                          </div>
+                        </td>
 
-                                                    <span className="font-medium text-xs">
-                                                        {item.selectedDeliveryAddress.fullName}</span>
-                                                    <span className="font-medium text-xs">
-                                                    {item.selectedDeliveryAddress.email}</span>
-                                                    <span className="font-medium text-xs">
-                                                    {item.selectedDeliveryAddress.phone}</span>
-                                                    <span className="font-medium text-xs">
-                                                    {item.selectedDeliveryAddress.selectedState}  &nbsp;
-                                                    {item.selectedDeliveryAddress.selectedCity} &nbsp;
-                                                    {item.selectedDeliveryAddress.selectedLocation} &nbsp;
-                                                    {item.selectedDeliveryAddress.street}&nbsp;
-                                                        {item.selectedDeliveryAddress.houseNumber}</span>
-                                                    <span className="bg-teal-500 px-3 py-2 mt-2 font-bold text-white">Payment:{item.selectedPaymentMethod }</span>
+                        <td className="py-3 px-6 flex flex-col text-center  whitespace-nowrap">
+                          <div className="flex flex-col items-center justify-center">
+                            <select className="bg-purple-200 rounded-full text-xs text-purple-700">
+                              <option value="Pending">Pending</option>
+                              <option value="Shipped">Shipped</option>
+                              <option value="Delivered">Delivered</option>
+                              <option value="Cancelled">Cancelled</option>
+                            </select>
+                          </div>
+                        </td>
 
-                                                </div>
-                                            </td>
-
-                                            <td className="py-3 px-6 text-center  whitespace-nowrap">
-                                                <div className="flex flex-col gap-5 items-center justify-center">
-
-                                                    
-                                                         <span  className="font-medium">
-                                                          {item.totalAmount}
-                                                        </span>
-                                                    
-
-                                                </div>
-                                            </td>
-
-                                            
-                                                <td  className="py-3 px-6 flex flex-col text-center  whitespace-nowrap">
-                                                                                            <div className="flex flex-col items-center justify-center">
-                                            
-                                                                                                
-                                                                                                        <select  className='bg-purple-200 rounded-full text-xs text-purple-700'>
-                                                                                                        <option value="Pending">Pending</option>
-                                                                                                        <option value="Shipped">Shipped</option>
-                                                                                                        <option value="Delivered">Delivered</option>
-                                                                                                        <option value="Cancelled">Cancelled</option>
-                                                                                                   </select>
-                                                                                                   
-                                                                                              
-                                            
-                                                                                            </div>
-                                                                                        </td>
-                                          
-
-
-                                            <td className="py-3 px-6 text-center  whitespace-nowrap">
-                                                <div className="flex flex-row gap-2 items-center justify-center">
-                                                        
-                                                    <AiOutlineEdit  className='w-6 h-6 cursor-pointer' />
-                                                 
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    ))
-                                    } </tbody>
-                                
-                              
-                            </table>
-                           
-                        </div>
-                        <Pagination page={Math.ceil(page)} setPage={setPage} totalPage={Math.floor(sellerOrders.length)} />
-                    </div>
-                </div>
+                        <td className="py-3 px-6 text-center  whitespace-nowrap">
+                          <div className="flex flex-row gap-2 items-center justify-center">
+                            <AiOutlineEdit className="w-6 h-6 cursor-pointer" />
+                          </div>
+                        </td>
+                      </tr>
+                    ))}{" "}
+                </tbody>
+              </table>
             </div>
-        </>
+            <MiniPagination
+              page={Math.ceil(page)}
+              setPage={setPage}
+              totalPage={Math.floor(sellerOrders.length)}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-    )
-}
-
-export default OrderManage
+export default OrderManage;
