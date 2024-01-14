22489988e04112690554b633f3be12f6d6b79798
parashar <parasharneupane3900@gmail.com>
2024-01-14 17:05:17 +0545

    Admin Product Edit Form Refactor

--- Client/src/Components/Pages/Seller/EditProduct.jsx
+++ Client/src/Components/Pages/Seller/EditProduct.jsx
@@ -12,12 +12,28 @@ import {
   selectProductById,
   updateProductAsync,
 } from "../../../features/product/productListSlice";
+import axios from "axios";
 import { data } from "autoprefixer";
 import { FaCheckCircle } from "react-icons/fa";
 import { IoIosClose } from "react-icons/io";
 import { selectLoggedInUserInfo } from "../../../features/user/userSlice";
+import ProductHighlightForm from "./ProductHighlightForm";
+import ProductFormFeatures from "./ProductFormFeatures";
+import ImageUploader from "../../Layout/ImageUploader";
 
 const EditProduct = () => {
+  const [imageURL, setImageURL] = useState("");
+  const [uploadedImages, setUploadedImages] = useState([]);
+  const [keywords, setKeywords] = useState([]);
+  const [keyword, setKeyword] = useState("");
+  const [feature1Title, setFeature1Title] = useState("");
+  const [feature1Option, setFeature1Option] = useState("");
+  const [feature1, setFeature1] = useState([]);
+  const [feature2Title, setFeature2Title] = useState("");
+  const [feature2Option, setFeature2Option] = useState("");
+  const [feature2, setFeature2] = useState([]);
+  const [highlightInput, setHighlightInput] = useState('');
+  const [highlights, setHighlights] = useState([]);
   const user = useSelector(selectLoggedInUserInfo);
   const seller = useSelector(selectLoggedInSeller);
   const params = useParams();
@@ -30,8 +46,7 @@ const EditProduct = () => {
     setValue,
     formState: { errors },
   } = useForm();
-  const [keywords, setKeywords] = useState([]);
-  const [keyword, setKeyword] = useState("");
+
 
   useEffect(() => {
     if (params.id) {
@@ -39,6 +54,50 @@ const EditProduct = () => {
     }
     console.log(selectedProduct);
   }, [params.id, dispatch, user]);
+
+  const addImageByUrl = async () => {
+    if (uploadedImages.length < 4) {
+      const { data } = await axios.post(
+        "http://localhost:8080/upload/upload-link",
+        { imageURL }
+      );
+      setUploadedImages((prev) => {
+        return [...prev, data.newName];
+      });
+    } else {
+      toast.error("Maximum 4 Uploads Allowed");
+    }
+  };
+
+  const addLocalImage = async (e) => {
+    e.preventDefault();
+    if (uploadedImages.length < 4) {
+      const files = e.target.files;
+      const data = new FormData();
+      for (let i = 0; i < files.length; i++) {
+        data.append("photos", files[i]);
+      }
+
+      await axios
+        .post("http://localhost:8080/upload", data, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((response) => {
+          const { data } = response;
+          setUploadedImages((prev) => [...prev, data.uploadedFiles[0]]);
+        });
+    } else {
+      toast.error("Maximum 4 Uploads Allowed");
+    }
+  };
+
+  const handleImageDelete = (id) => {
+    setUploadedImages(uploadedImages.filter((item, index) => index !== id));
+  };
+
+  const handleHighlightChange = (e) => {
+    setHighlightInput(e.target.value);
+  }
 
   useEffect(() => {
     if (selectedProduct) {
@@ -52,9 +111,13 @@ const EditProduct = () => {
       setValue("category", selectedProduct.category);
       setValue("thumbnail", selectedProduct.thumbnail);
       setValue("brand", selectedProduct.brand);
-      setValue("image1", selectedProduct.images[0]);
-      setValue("image2", selectedProduct.images[1]);
-      setValue("image3", selectedProduct.images[2]);
+      setUploadedImages(selectedProduct.images);
+      setHighlightInput(selectedProduct.highlights);
+      setFeature1Title(selectedProduct.features[0].title);
+      setFeature1(selectedProduct.features[0].options);
+      setFeature2Title(selectedProduct.features[1].title);
+      setFeature2(selectedProduct.features[1].options);
+
       console.log(selectedProduct);
     }
   }, [selectedProduct]);
@@ -64,34 +127,34 @@ const EditProduct = () => {
       {!user && <Navigate to={"/"} replace={true}></Navigate>}
       {!seller && <Navigate to={"/"} replace={true}></Navigate>}
       <div className="h-screen  flex flex-col  items-center ">
-        <form
+      <form
           noValidate
-          onSubmit={handleSubmit((data) => {
-            const datas = { ...data };
-            datas.id = params.id;
-            datas.keywords =keywords;
-            datas.images = [
-              datas.image1,
-              datas.image2,
-              datas.image3,
-              datas.thumbnail,
-            ];
-            datas.images = [
-              datas.image1,
-              datas.image2,
-              datas.image3,
-              datas.thumbnail,
-            ];
-            datas.rating = 0;
-            datas.seller = user.id;
-            delete datas["image1"];
-            delete datas["image2"];
-            delete datas["image3"];
-            console.log(datas);
-            dispatch(updateProductAsync(datas));
+          onSubmit={handleSubmit(async(data) => {
+            const product = {
+              ...data,
+            };
+          
+            if (keywords.length <= 0) {
+              toast.error("Enter Keywords")
+              return
+            }
+            if (uploadedImages.length < 4) {
+              toast.error("Please Upload at least 4 images")
+              return
+            }
+            product.id = params.id;
+            product.thumbnail = uploadedImages[0];
+            product.images = [...uploadedImages];
+            product.keywords = keywords;
+            product.rating = 0;
+            product.seller = seller.id;
+            product.features =[{title:feature1Title,options:[...feature1]},{title:feature2Title,options:[...feature2]}]
+            product.highlights = highlightInput;
+            console.log(product);
+           await dispatch(updateProductAsync(product));
             navigate("/sellerOptions/seller-Dashboard/manage-products/");
           })}
-          className="mt-5 shadow-lg px-5 py-3 bg-white"
+          className="mt-5 w-3/4 shadow-lg px-5 py-3 bg-white"
         >
           <div className="space-y-12">
             <div className="border-b border-gray-900/10 pb-12">
@@ -148,6 +211,9 @@ const EditProduct = () => {
                   </p>
                 </div>
 
+                <ProductHighlightForm handleHighlightChange={handleHighlightChange} highlightInput={highlightInput} />
+
+
                 <div className="col-span-full">
                   <label
                     htmlFor="about"
@@ -163,6 +229,16 @@ const EditProduct = () => {
                         setKeyword(e.target.value);
                         console.log(keyword);
                       }}
+                      onKeyDown={(e) => {
+                        if (e.key == "Enter") {
+                          if (keywords.length < 10) {
+                            setKeywords([...keywords, keyword]);
+                          setKeyword("");
+                          } else {
+                            toast.error("Maximum 10 Keywords Allowed")
+                          }
+                        }
+                      }}
                       rows="3"
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
@@ -170,13 +246,18 @@ const EditProduct = () => {
                       size={20}
                       className="absolute right-7 top-2 cursor-pointer text-green-700"
                       onClick={() => {
-                        setKeywords([...keywords, keyword]);
-                        setKeyword("");
+                        if (keywords.length < 10) {
+                          setKeywords([...keywords, keyword]);
+                          setKeyword("");
+                        } else {
+                          toast.error("Maximum 10 Keywords Allowed")
+                        }
+                        
                       }}
                     />
                   </div>
                   {keywords.length <= 0 ? (
-                    <p className="bg-[#b13333] w-fit text-white font-bold mt-4 py-2 px-3">
+                    <p className="bg-[#b13333] text-white w-fit  font-bold mt-4 py-1 px-3">
                       Enter Some Keywords
                     </p>
                   ) : (
@@ -203,6 +284,9 @@ const EditProduct = () => {
                     </div>
                   )}
                 </div>
+
+                {/* Product Form Features Adder */}
+                <ProductFormFeatures feature1Title={feature1Title} feature1={feature1} feature2Title={feature2Title} feature2={feature2} setFeature1Title={setFeature1Title} setFeature2Title={setFeature2Title} setFeature1={setFeature1} setFeature2={setFeature2} feature1Option={feature1Option} setFeature1Option={setFeature1Option} feature2Option={feature2Option} setFeature2Option={setFeature2Option} register={register} />
 
                 <div className="sm:col-span-2">
                   <label
@@ -252,115 +336,25 @@ const EditProduct = () => {
                     </div>
                   </div>
                 </div>
-                <div className="sm:col-span-2">
+                <div className="col-span-full">
                   <label
-                    htmlFor="username"
+                    htmlFor="about"
                     className="block text-sm font-medium leading-6 text-gray-900"
                   >
-                    Product Stock
+                    Images
                   </label>
-                  <div className="mt-2">
-                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                      <input
-                        type="number"
-                        {...register("stock", {
-                          required: "Stock is Required",
-                          min: 1,
-                        })}
-                        id="stock"
-                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                        placeholder="Product Stock Here"
-                      />
-                    </div>
-                  </div>
-                </div>
+                  <p className="mt-3 text-sm leading-6 text-gray-600">
+                    Note: Your First Image Acts as Thumbnail
+                  </p>
 
-                <div className="sm:col-span-6">
-                  <label
-                    htmlFor="username"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Product Thumbnail URL
-                  </label>
-                  <div className="mt-2">
-                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                      <input
-                        type="text"
-                        {...register("thumbnail", {
-                          required: "thumbnail is Required",
-                        })}
-                        id="thumbnail"
-                        autoComplete="title"
-                        className="w-full block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                        placeholder="Paste Product Thumbnail Here"
-                      />
-                    </div>
-                  </div>
-                </div>
-
-                <div className="sm:col-span-6">
-                  <label
-                    htmlFor="username"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Image 1
-                  </label>
-                  <div className="mt-2">
-                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                      <input
-                        type="text"
-                        {...register("image1", {
-                          required: "Image1 is Required",
-                        })}
-                        id="image1"
-                        className="w-full block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                        placeholder="Paste Image1 URL Here"
-                      />
-                    </div>
-                  </div>
-                </div>
-
-                <div className="sm:col-span-6">
-                  <label
-                    htmlFor="image2"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Image 2
-                  </label>
-                  <div className="mt-2">
-                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                      <input
-                        type="text"
-                        {...register("image2", {
-                          required: "Image2 is Required",
-                        })}
-                        id="image2"
-                        className="w-full block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                        placeholder="Paste Image2 URL Here"
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className="sm:col-span-6">
-                  <label
-                    htmlFor="image3"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Image 3
-                  </label>
-                  <div className="mt-2">
-                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                      <input
-                        type="text"
-                        {...register("image3", {
-                          required: "Image3 is Required",
-                        })}
-                        id="image3"
-                        className="w-full block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                        placeholder="Paste Image3 URL Here"
-                      />
-                    </div>
-                  </div>
+                  <ImageUploader
+                    addImageByUrl={addImageByUrl}
+                    handleImageDelete={handleImageDelete}
+                    imageURL={imageURL}
+                    setImageURL={setImageURL}
+                    uploadedImages={uploadedImages}
+                    addLocalImage={addLocalImage}
+                  />
                 </div>
 
                 <div className="sm:col-span-3">
@@ -430,9 +424,12 @@ const EditProduct = () => {
             </button>
             <button
               type="submit"
+              onClick={() => {
+                handleAddHighLight()
+              }}
               className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Edit
+              Add
             </button>
           </div>
         </form>
